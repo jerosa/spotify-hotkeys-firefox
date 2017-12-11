d191f5b6c0e2554e72c6f30327cea2cf587f6141
TsunDoge <espinosajesus8@gmail.com>
2017-12-11 12:56:28 +0100

    First version

---
+++ background.js
@@ -0,0 +1,42 @@
+/**
+ * Fired when a registered command is activated using a keyboard shortcut.
+ */
+browser.commands.onCommand.addListener((command) => {
+    browser.tabs.query({ url: 'https://*.spotify.com/*' }, (tabs) => {
+
+        // Open a spotify tab if one does not exist yet.
+        if (tabs.length === 0) {
+            browser.tabs.create({ url: 'https://open.spotify.com' });
+        }
+
+        // Apply command
+        for (let tab of tabs) {
+            let code = "";
+            if (tab.url.startsWith('https://play.spotify.com')) {
+                code = "document.getElementById('app-player').contentDocument.getElementById('" + command + "').click()";
+            }
+            else if (tab.url.startsWith('https://open.spotify.com')) {
+                switch (command) {
+                    case "play-pause": 
+                        code = '(document.querySelector(".spoticon-play-16") || document.querySelector(".spoticon-pause-16")).click()'; 
+                        break;
+                    case "next":
+                        code = 'document.querySelector(".spoticon-skip-forward-16").click()';
+                        break;
+                    case "previous":
+                        code = 'document.querySelector(".spoticon-skip-back-16").click()';
+                        break;
+                    case "shuffle":
+                        code = 'document.querySelector(".spoticon-shuffle-16").click()';
+                        break;
+                    case "repeat": 
+                        code = 'document.querySelector(".spoticon-repeat-16").click()';
+                        break;
+                }
+            }
+            if (code.length) {
+                browser.tabs.executeScript(tab.id, {code: code});
+            }
+        }
+    });
+});
