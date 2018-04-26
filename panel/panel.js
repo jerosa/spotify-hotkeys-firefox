fa3a1f07177d51d2e1c0451c69187b4419ad7d56
TsunDoge <espinosajesus8@gmail.com>
2018-04-26 22:40:09 +0200

    Fix: Custom shortcuts now are shown on panel

--- panel/panel.js
+++ panel/panel.js
@@ -1,35 +1,39 @@
-const manifest = browser.runtime.getManifest();
-const commands = manifest.commands;
-
-function sendCommand(ev) {
-    const command = ev.target.dataset.command;
-    if (command in commands) {
-        browser.runtime.sendMessage(command);
+async function init() {
+    const manifest = browser.runtime.getManifest();
+    const commands = await browser.commands.getAll();
+    
+    function sendCommand(ev) {
+        const command = ev.target.dataset.command;
+        if (command !== undefined) {
+            browser.runtime.sendMessage(command);
+        }
     }
+    
+    function createCommandMarkup(commandName, command) {
+        const li = document.createElement("li");
+        const p1 = document.createElement("p");
+        p1.dataset.command = `${commandName}`;
+        p1.appendChild(document.createTextNode(`${commandName} - ${command.shortcut}`));
+        li.appendChild(p1);
+        const p2 = document.createElement("p");
+        const small = document.createElement("small");
+        small.appendChild(document.createTextNode(`${command.description}`));
+        p2.appendChild(small);
+        li.appendChild(p2);
+        li.addEventListener("click", sendCommand);
+        return li;
+    }
+
+    // Set title
+    const content = document.querySelector("#title");
+    content.appendChild(document.createTextNode(manifest.name));
+    
+    // Create the commands list
+    const commandsFragment = document.createDocumentFragment();
+    for (const command in commands) {
+        commandsFragment.appendChild(createCommandMarkup(commands[command].name, commands[command]));
+    }
+    document.querySelector("#commands").appendChild(commandsFragment);
 }
 
-function createCommandMarkup(commandName, command) {
-    const li = document.createElement("li");
-    const p1 = document.createElement("p");
-    p1.dataset.command = `${commandName}`;
-    p1.appendChild(document.createTextNode(`${commandName} - ${command.suggested_key.default}`));
-    li.appendChild(p1);
-    const p2 = document.createElement("p");
-    const small = document.createElement("small");
-    small.appendChild(document.createTextNode(`${command.description}`));
-    p2.appendChild(small);
-    li.appendChild(p2);
-    li.addEventListener("click", sendCommand);
-    return li;
-}
-
-// Set title
-const content = document.querySelector("#title");
-content.appendChild(document.createTextNode(manifest.name));
-
-// Create the commands list
-const commandsFragment = document.createDocumentFragment();
-for (let command in commands) {
-    commandsFragment.appendChild(createCommandMarkup(command, commands[command]));
-}
-document.querySelector("#commands").appendChild(commandsFragment);
+init();
