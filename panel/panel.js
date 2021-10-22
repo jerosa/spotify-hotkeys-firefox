4e40d93817c4a650bb9b24777113b032c7581f1d
mmustra <marin.mustra@gmail.com>
2021-10-22 19:46:10 +0200

    Target "li" element for command execution

--- panel/panel.js
+++ panel/panel.js
@@ -4,7 +4,7 @@ async function init() {
     const commands = await browser.commands.getAll();
 
     function sendCommand(ev) {
-        const { command } = ev.target.dataset;
+        const { command } = ev.target.closest("li").dataset;
         if (command !== undefined) {
             browser.runtime.sendMessage(command);
         }
@@ -12,8 +12,8 @@ async function init() {
 
     function createCommandMarkup(commandName, command) {
         const li = document.createElement("li");
+        li.dataset.command = `${commandName}`;
         const p1 = document.createElement("p");
-        p1.dataset.command = `${commandName}`;
         p1.appendChild(document.createTextNode(`${commandName} - ${command.shortcut}`));
         li.appendChild(p1);
         const p2 = document.createElement("p");
